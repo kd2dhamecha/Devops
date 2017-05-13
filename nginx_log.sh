33dab6d296620ee1560621239d7cb0d6f4b44ac1
kd2dhamecha <kapil.dhamecha@gmail.com>
2017-05-13 20:44:32 +0530

    Update nginx_log.sh

--- nginx_log.sh
+++ nginx_log.sh
@@ -13,12 +13,13 @@ fi
 if [ "$1" = 'URL' ]; then
 awk -F\" '{print $2}' $LogFile | awk '{print $2}' | sort | uniq -c | sort -r > ./result/Mostrequested_url_stat
 fi
+if [ "$1" = 'RATIO' ]; then
 cat $LogFile  | grep -i "MISS" > ./result/Miss_stat
 cat $LogFile  | grep -i "HIT" > ./result/Hit_stat
-
 MISS=`cat $LogFile  | grep -i "MISS" | wc -l`
 echo "MISS : $MISS"
 HIT=`cat $LogFile  | grep -i "HIT" | wc -l`
 echo "HIT : $HIT"
 RATIO=$((HIT/MISS))
-echo "HIT/MISS RATIO : $RATIO" 
+echo "HIT/MISS RATIO : $RATIO"
+fi 
