d052824f394eb16f820462c9ac774e0a1a187ada
kd2dhamecha <kapil.dhamecha@gmail.com>
2018-09-09 17:32:14 +0530

    test

---
+++ src/org/foo/bar.groovy
@@ -0,0 +1,27 @@
+package org.foo
+
+node {
+
+            stage('Checkout source') {
+                deleteDir()
+                checkout scm
+            }
+
+
+def mvn(args) {
+  mvn(args, false)
+}
+
+def mvn(args, ignoreFailures) {
+
+  def mvnHome = tool 'maven 3.3.9'
+  env.PATH = "${mvnHome}/bin:${env.PATH}"
+
+  if (ignoreFailures) {
+    sh "set +e; mvn -B ${args} -Dexternal.version-qualifier=${extVerQualifier()}; set -e"
+  } else {
+    sh "mvn -B ${args} -Dexternal.version-qualifier=${extVerQualifier()}"
+  }
+
+}
+}
