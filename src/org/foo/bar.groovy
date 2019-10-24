9768c4299d32ddcf2d1fd4d7970b67a037b97701
kapil <kapil.dhamecha@gmail.com>
2019-10-24 18:58:12 +0530

    Update bar.groovy

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -1,30 +1,5 @@
 package org.foo
 
 def build() {
-
-node {
-
-            stage('Checkout source') {
-                deleteDir()
-                checkout scm
-            }
-
-
-def mvn(args) {
-  mvn(args, false)
-}
-
-def mvn(args, ignoreFailures) {
-
-  def mvnHome = tool 'maven 3.3.9'
-  env.PATH = "${mvnHome}/bin:${env.PATH}"
-
-  if (ignoreFailures) {
-    sh "set +e; mvn -B ${args} -Dexternal.version-qualifier=${extVerQualifier()}; set -e"
-  } else {
-    sh "mvn -B ${args} -Dexternal.version-qualifier=${extVerQualifier()}"
-  }
-
-}
-}
+echo "hi test shared lib"
 }
